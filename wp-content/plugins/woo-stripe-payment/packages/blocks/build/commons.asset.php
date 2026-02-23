fa16b907ca66e7d82e5b9b88526bb4c51c054425
parthgoodmans <pdshah3690@gmail.com>
2026-02-23 19:16:22 +0530

    Payment Plugins for Stripe WooCommerce plugin

---
+++ wp-content/plugins/woo-stripe-payment/packages/blocks/build/commons.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '3b9fad00244121522505');
---
+++ wp-content/plugins/woo-stripe-payment/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '3.3.94.x-dev',
+        'version' => '3.3.94.9999999-dev',
+        'reference' => 'aad078b3df7bb88710a9d68e0ac1e204475f2cb2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '3.3.94.x-dev',
+            'version' => '3.3.94.9999999-dev',
+            'reference' => 'aad078b3df7bb88710a9d68e0ac1e204475f2cb2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v13.18.0',
+            'version' => '13.18.0.0',
+            'reference' => '02abb043b103766f4ed920642ae56ffdc58c7467',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
